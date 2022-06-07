a7a11bc60230e26e7c51dc45b202097bf8292b57
jessefontaine <97663063+jessefontaine@users.noreply.github.com>
2022-06-07 11:30:08 +0200

    Update loader.py

--- rush_hour/loader.py
+++ rush_hour/loader.py
@@ -1,6 +1,12 @@
-import pandas as pd
+# import pandas as pd
 
-# saves data from input file to dataframe
-data_df = pd.read_csv("game_boards/Rushhour6x6_1.csv")
+# # saves data from input file to dataframe
+# data_df = pd.read_csv("game_boards/Rushhour6x6_1.csv")
 
-print(data_df)
+# print(data_df)
+
+def loader(filepath):
+    
+    with open(filepath) as file:
+
+        line = file.readline()
