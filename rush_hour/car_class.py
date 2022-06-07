2cf417e74d72a7e8bddbf89459e72454eb6a2014
jessefontaine <jesse.fontaine98@gmail.com>
2022-06-07 15:14:24 +0200

    move method

--- rush_hour/car_class.py
+++ rush_hour/car_class.py
@@ -1,5 +1,5 @@
-from typing import Union, Tuple
 from __future__ import annotations
+from typing import Union, Tuple
 
 class Car():
     
@@ -17,3 +17,18 @@ class Car():
         self.orientation: str = orientation
         self.length: int = int(length)
 
+    def move(self: Car, move: int) -> None:
+        """
+            Change car position on gameboard. 
+
+            Move can be a positive or negative integer. Positive values will move the car
+            up or to the right, while negative integers will move down or to the left (all
+            with respect to cars orientation).
+        """
+
+        # change position depending on cars orientation
+        if self.orientation == 'H':
+            self.position = (self.position[0] + move, self.position[1])
+
+        elif self.orientation == 'V':
+            self.position = (self.position[0], self.position[1] + move)
