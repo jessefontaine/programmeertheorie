0a7b6b16cc2d46972bb2da1c2e1c18353a22238d
Laura <lhaverkorn@github.com>
2022-06-07 13:58:02 +0200

    speciaal voor Jesse

--- rush_hour/game_board.py
+++ rush_hour/game_board.py
@@ -4,26 +4,35 @@ import re
 class Board():
 
     def __init__(self, filepath):
-        grid_digits = re.findall("[0-9]x[0-9]", filepath)
-        grid_digits = grid_digits[0].split('x')
+        self.grid_digits = re.findall("[0-9]x[0-9]", filepath)
+        self.grid_digits = self.grid_digits[0].split('x')
 
-        gridline = []
-        for i in range(int(grid_digits[0])):
-            gridline.append(" ")
-        grid = []
-        for i in range(int(grid_digits[1])):
-            grid.append(list(gridline))
+        self.gridline = []
+        for i in range(int(self.grid_digits[0])):
+            self.gridline.append(" ")
+        self.grid = []
+        for i in range(int(self.grid_digits[1])):
+            self.grid.append(list(self.gridline))
 
-        car_list = loader(filepath)
+        self.car_list = loader(filepath)
 
-        for car in car_list:
+        for car in self.car_list:
             for i in range(car.length):
                 if car.orientation == "H":
-                    grid[car.position[0] - 1][car.position[1] + i - 1] = car.name
+                    self.grid[car.position[0] - 1][car.position[1] + i - 1] = car.name
                 else:
-                    grid[car.position[0] + i - 1][car.position[1] - 1] = car.name
+                    self.grid[car.position[0] + i - 1][car.position[1] - 1] = car.name
+        print(self.grid)
 
-        print(grid)
+    def possible_moves(self):
+        for car in self.car_list:
+            if car.orientation == "H":
+                print(self.grid[car.position[0]][car.position[1] - 1], "SPACE LEFT")
+
+                print(car.position, "HORIZONTAL")
+            else:
+                print(car.position, "VERTICAL")
 
 if __name__ == "__main__":
     a = Board("game_boards/Rushhour6x6_1.csv")
+    a.possible_moves()
