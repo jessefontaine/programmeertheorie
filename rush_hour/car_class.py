c82acbd4c4c110cfc0d988020128c051f16452a6
Annemarie88 <ageertsema@upcmail.nl>
2022-06-07 12:43:17 +0200

    function load_board, made grid and added cars

--- rush_hour/car_class.py
+++ rush_hour/car_class.py
@@ -2,7 +2,7 @@ class Car():
     
     def __init__(self, name, orientation, col, row, length):
         self.name = name
-        self.position = (row, col)
+        self.position = (int(row), int(col))
         self.orientation = orientation
         self.length = length
 
--- rush_hour/game_board.py
+++ rush_hour/game_board.py
@@ -1,6 +1,30 @@
-
+from loader import loader
+import re
 
 class Board():
 
     def __init__(self):
         pass
+
+    def load_board(self, filepath):
+        grid_digits = re.findall("[0-9]x[0-9]", filepath)
+        grid_digits = grid_digits[0].split('x')
+
+        gridline = []
+        for i in range(int(grid_digits[0])):
+            gridline.append(" ")
+        grid = []
+        for i in range(int(grid_digits[1])):
+            grid.append(list(gridline))
+
+        car_list = loader("game_boards/Rushhour6x6_1.csv")
+
+        for car in car_list:
+            print(car.position)
+            grid[car.position[0] - 1][car.position[1] - 1] = car.name
+
+        print(grid)
+
+if __name__ == "__main__":
+    a = Board()
+    a.load_board("game_boards/Rushhour6x6_1.csv")
