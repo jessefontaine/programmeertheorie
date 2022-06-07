f644aa57d5ced0065811aa9954a3ba6b3a471108
jessefontaine <jesse.fontaine98@gmail.com>
2022-06-07 14:24:04 +0200

    merge all

--- rush_hour/car_class.py
+++ rush_hour/car_class.py
@@ -1,8 +1,19 @@
+from typing import Union, Tuple
+from __future__ import annotations
+
 class Car():
     
-    def __init__(self, name, orientation, col, row, length):
-        self.name = name
-        self.position = (int(row) - 1, int(col) - 1)
-        self.orientation = orientation
-        self.length = int(length)
+    def __init__(
+        self: Car, 
+        name: str, 
+        orientation: str, 
+        col: Union[int, str], 
+        row: Union[int, str], 
+        length: Union[int, str]
+        ) -> None:
+
+        self.name: str = name
+        self.position: Tuple[int, int] = (int(row) - 1, int(col) - 1)
+        self.orientation: str = orientation
+        self.length: int = int(length)
 
