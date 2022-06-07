7b49f7b9121896926ceeee5d2725c2d844b84a10
jessefontaine <jesse.fontaine98@gmail.com>
2022-06-07 16:18:29 +0200

    ding

--- rush_hour/game_board.py
+++ rush_hour/game_board.py
@@ -11,23 +11,10 @@ class Board():
         # parse board size from filename
         size = re.findall("[0-9]x[0-9]", filepath)[0].split('x')
         self.size = (int(size[0]), int(size[1]))
-
-        
-        self.gridline = []
-        for i in range(int(self.size[0])):
-            self.gridline.append(" ")
-        self.grid = []
-        for i in range(int(self.size[1])):
-            self.grid.append(list(self.gridline))
-
         self.car_list = loader(filepath)
 
-        for car in self.car_list:
-            for i in range(car.length):
-                if car.orientation == "H":
-                    self.grid[car.position[0]][car.position[1] + i] = car.name
-                else:
-                    self.grid[car.position[0] + i][car.position[1]] = car.name
+        
+        
         print(self.grid)
 
     def possible_moves(self):
@@ -63,6 +50,23 @@ class Board():
 
     def random_final_move(self, dict):
         return random.choice(list(dict.items()))
+    
+    def update_grid(self):
+        
+        self.gridline = []
+        for i in range(int(self.size[0])):
+            self.gridline.append(" ")
+        self.grid = []
+        for i in range(int(self.size[1])):
+            self.grid.append(list(self.gridline))
+
+
+        for car in self.car_list:
+            for i in range(car.length):
+                if car.orientation == "H":
+                    self.grid[car.position[0]][car.position[1] + i] = car.name
+                else:
+                    self.grid[car.position[0] + i][car.position[1]] = car.name
 
     def update_grid(self, car_move):
         print(car_move)
