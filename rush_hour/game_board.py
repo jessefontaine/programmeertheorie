81e6d4660c92ee6e3e0db4878f492a02beedf8a1
jessefontaine <jesse.fontaine98@gmail.com>
2022-06-07 13:38:39 +0200

    huilen

--- rush_hour/game_board.py
+++ rush_hour/game_board.py
@@ -3,10 +3,7 @@ import re
 
 class Board():
 
-    def __init__(self):
-        pass
-
-    def load_board(self, filepath):
+    def __init__(self, filepath):
         grid_digits = re.findall("[0-9]x[0-9]", filepath)
         grid_digits = grid_digits[0].split('x')
 
@@ -29,5 +26,4 @@ class Board():
         print(grid)
 
 if __name__ == "__main__":
-    a = Board()
-    a.load_board("game_boards/Rushhour6x6_1.csv")
+    a = Board("game_boards/Rushhour6x6_1.csv")
