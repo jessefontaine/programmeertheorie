c67232ea15c1a05c4624cd806b6f706c8118716f
jessefontaine <jesse.fontaine98@gmail.com>
2022-06-10 13:48:07 +0200

    pull

--- rush_hour/run.py
+++ rush_hour/run.py
@@ -21,9 +21,9 @@ amt_list = batch_run("game_boards/Rushhour6x6_1.csv", 10000)
 
 # plot all runs
 plt.hist(amt_list, density=True)
-plt.savefig("plots/Rushour6x6_1_10.png")
+plt.savefig("plots/Rushour6x6_1_10000.png")
 
 # convert to str and write to file
 amt_list = [str(x) for x in amt_list]
-with open('batch_run.txt', 'w') as file:
+with open('batch_run_1_10000.txt', 'w') as file:
     file.write('\n'.join(amt_list))
