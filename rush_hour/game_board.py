37fdf130c93eaa51e0204dbf05754201f235151a
Laura <lhaverkorn@github.com>
2022-06-10 13:10:36 +0200

    X

--- rush_hour/game_board.py
+++ rush_hour/game_board.py
@@ -10,7 +10,7 @@ class Board():
     def __init__(self: Board, filepath: str) -> None:
 
         # parse board size from filename
-        size = re.findall("[0-9]x[0-9]", filepath)[0].split('x')
+        size = re.findall("[0-9]+x[0-9]+", filepath)[0].split('x')
         self.size: Tuple[int, int] = (int(size[0]), int(size[1]))
 
         self.car_list: List[Car] = loader(filepath)
@@ -206,11 +206,10 @@ class Board():
         
 
 if __name__ == "__main__":
-    a = Board("game_boards/Rushhour6x6_1.csv")
+    a = Board("game_boards/Rushhour12x12_7.csv")
 
     a.step_random()
 
     import pandas
-
     pandas.DataFrame(a.moves_made, columns=['car', 'move']).to_csv('output.csv', index=False)
     # print(moves)
