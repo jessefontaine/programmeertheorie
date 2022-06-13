c84d74c7b5df7ad6edcd1e9d18c027b3461dff1a
Marianne Geertsema <rinto@MacBook-Pro-van-Marianne.local>
2022-06-13 13:27:01 +0200

    cleaned up random_alg

--- code/algorithms/random_alg.py
+++ code/algorithms/random_alg.py
@@ -1,14 +1,15 @@
 from __future__ import annotations
+from ..classes import Board, Car
 from typing import Tuple, List
 import random
+
 
 class Random_Alg():
 
     def __init__(self: Random_Alg, board: Board) -> None:
         self.board = board
 
-
-    def move(self: Random_Alg, dict: dict[Car, List[int]]):
+    def move(self: Random_Alg, dict: dict[Car, List[int]]) -> Tuple[Car, int]:
         """
             Returns a random move; a tuple with car object and the direction.
             Requires a dictionary with all possible moves.
@@ -20,8 +21,12 @@ class Random_Alg():
 
         return car_move[0], ran_choice
 
-    def step(self):
+    def step(self) -> None:
+        """
+            Runs the random algoritm until the game is won.
+        """
 
+        # make random steps in game untill red car is in win position
         while not self.board.win():
             pos_moves = self.board.possible_moves()
 
