771ad0ae0b3a8b3b63ba1a21cd35c8e94b8e6822
Laura <lhaverkorn@github.com>
2022-06-13 14:38:05 +0200

    more bins in plots

--- plot.py
+++ plot.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-with open("batch_run_4_10000.txt", "r") as f:
+with open("output/12x12_7_random/Rushhour12x12_7_random_1000.txt", "r") as f:
     lines = f.readlines()
 
 new_list = [int(i.split('\n', 1)[0]) for i in lines]
@@ -9,5 +9,5 @@ print(new_list)
 print(max(new_list))
 
 plt.hist(new_list, density=True, bins=50)
-plt.savefig("plots/Rushour9x9_5_10000.png")
+plt.savefig("output/12x12_7_random/Rushhour12x12_7_random_1000.png")
 
