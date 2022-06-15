0bff90c75d130d747f8fb4f3680d9a992dc2a030
jessefontaine <jesse.fontaine98@gmail.com>
2022-06-15 15:18:01 +0200

    jezus wat was de vorige psuh lang geleden

--- code/algorithms/node.py
+++ code/algorithms/node.py
@@ -5,8 +5,8 @@ from typing import List, Tuple
 
 class Node():
 
-    def __init__(self: Node, board_rep: str, steps: List[Tuple[str, int]] = []):
+    def __init__(self: Node, board_rep: str):
         
         self.board_rep: str = board_rep
-        self.steps_taken: List[Tuple[str, int]] = steps
+        self.steps_taken: List[Tuple[str, int]] = []
         self.possible_moves: List[Tuple[str, int]] = []
