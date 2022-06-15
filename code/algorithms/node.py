3c3530fa8164c64da4bb2652ade28a828ea33414
jessefontaine <jesse.fontaine98@gmail.com>
2022-06-15 12:54:22 +0200

    node.py revisage

--- code/algorithms/node.py
+++ code/algorithms/node.py
@@ -5,8 +5,8 @@ from typing import List, Tuple
 
 class Node():
 
-    def __init__(self: Node, board_rep: str):
+    def __init__(self: Node, board_rep: str, steps: List[Tuple[str, int]] = []):
         
         self.board_rep: str = board_rep
-        self.steps_taken: List[Tuple[str, int]] = []
+        self.steps_taken: List[Tuple[str, int]] = steps
         self.possible_moves: List[Tuple[str, int]] = []
