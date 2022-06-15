170bce0d404419e21340bad87b4db00c885ba72b
Marianne Geertsema <rinto@MacBook-Pro-van-Marianne.local>
2022-06-15 12:34:42 +0200

    deep alg with pruning

--- test.py
+++ test.py
@@ -1,25 +1,25 @@
-# from code.algorithms import First_Alg, Random_Alg, Breadth_Alg, Deep_Alg
-# from code.classes import Board
+from code.algorithms import First_Alg, Random_Alg, Breadth_Alg, Deep_Alg
+from code.classes import Board
 
-# a = Board("game_boards/Rushhour6x6_1.csv")
+a = Board("game_boards/Rushhour6x6_1.csv")
 
-# print('AAAAAAAAAAAAAAAAAAAAAAAAAA\n',a, '\nAAAAAAAAAAAAAAAA')
+#print('AAAAAAAAAAAAAAAAAAAAAAAAAA\n',a, '\nAAAAAAAAAAAAAAAA')
 
-# b = Deep_Alg(a)
+b = Deep_Alg(a)
 
-# b.run_algorithm()
-
-
-setup_str: str = '.AABBB\n.CCEDD\nXXGE..\nFFGHHI\nK.LJJI\nK.L...'
-setup_str = setup_str.replace('\n', '')
+b.run_algorithm()
 
 
-
-# get a list of all car names
-car_names_repeats = setup_str.replace('.', '')
-cars = list(set(car_names_repeats))
+# setup_str: str = '.AABBB\n.CCEDD\nXXGE..\nFFGHHI\nK.LJJI\nK.L...'
+# setup_str = setup_str.replace('\n', '')
 
 
 
-print(cars)
+# # get a list of all car names
+# car_names_repeats = setup_str.replace('.', '')
+# cars = list(set(car_names_repeats))
+
+
+
+# print(cars)
 
