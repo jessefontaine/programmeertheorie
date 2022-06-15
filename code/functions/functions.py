82474b3a693eda21afb592807b057b81b61ce409
Laura <lhaverkorn@github.com>
2022-06-15 16:05:25 +0200

    circular import error solved

--- code/functions/functions.py
+++ code/functions/functions.py
@@ -3,10 +3,10 @@ from __future__ import annotations
 from matplotlib import pyplot as plt
 from typing import List, Tuple, Union
 
-from code.algorithms import Random_Alg, First_Alg, Bfs, Depth_Alg
+# from code.algorithms import Random_Alg, First_Alg, Bfs, Depth_Alg
 # from code.classes import Board, Car
 
-def batch_runner(algorithm: Union[Random_Alg, First_Alg, Bfs, Depth_Alg], runs: int):
+def batch_runner(algorithm: Union["Random_Alg", "First_Alg", "Bfs", "Depth_Alg"], runs: int):
     
     amount_moves_per_runs: List[int] = []
     moves_made_in_runs: List[List[Tuple[str, int]]] = []
