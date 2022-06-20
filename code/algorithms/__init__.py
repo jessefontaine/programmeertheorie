a6a21692341351748cad98f7bb485a2515209ce4
Annemarie <annemarie@MacBook-Air-van-Annemarie.local>
2022-06-20 16:00:10 +0200

    random alg wich saves all states

--- code/algorithms/__init__.py
+++ code/algorithms/__init__.py
@@ -1,5 +1,6 @@
 import imp
 from .random_alg import Random_Alg
+from .random_alg_new import RandomAlg
 from .bfs import Bfs
 from .dfs import Dfs
 from .bdfs import Bdfs
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import os
 from typing import Union, List, Tuple
 
 from code.classes import Board
-from code.algorithms import Random_Alg, Bfs, Dfs, Bdfs
+from code.algorithms import RandomAlg, Bfs, Dfs, Bdfs
 from code.functions import batch_runner, plot_steps_to_file, steps_amount_to_file, write_moves_to_file
 
 
@@ -15,7 +15,7 @@ def main(infile: str, outfolder: str, mode: str, runs: int, output_moves: bool):
     board: Board = Board(infile)
 
     if mode == 'random':
-        algorithm: Union[Random_Alg, Bfs, Dfs, Bdfs] = Random_Alg(board)
+        algorithm: Union[RandomAlg, Bfs, Dfs, Bdfs] = RandomAlg(board)
     elif mode == 'first':
         # algorithm = First_Alg(board)
         pass
