af1abf93358b0b61939ab3a4f22a0dd1e5e8adc8
Annemarie <annemarie@MacBook-Air-van-Annemarie.local>
2022-06-20 13:33:46 +0200

    .

--- code/algorithms/tree_search.py
+++ code/algorithms/tree_search.py
@@ -1,21 +1,21 @@
 from __future__ import annotations
 from typing import Tuple, List, Set, Union
 from code.classes import Board
+from .node import Node
 from code.functions.functions import write_moves_to_file
-from code.classes import Node
+from .node import Node
 
 
 class Treesearcher:
   
-    def __init__(self, board: Board, depth: int, start_node: Union[Node, None] = None, end_node: Union[Node, None] = None):
+    def __init__(self, board: Board, depth: int, start_node: Union[Node, None] = None, end_node: Union[Node, None]):
         self.board: Board = board
         self.depth: int = depth
         
-        if start_node is None:
-            self.start_node: Node = Node(str(board))
+        if start_node is not None:
+            self.start_node: Node = start_node
         else:
-            self.start_node = start_node
-
+            self.start_node = Node(str(board))
         if end_node is not None:
             self.end_node: Node = end_node
         
@@ -23,37 +23,12 @@ class Treesearcher:
     
     
     def get_current_state(self):
-        raise NotImplementedError
-
-    def build_children(self, parent: Node):
-        self.board.set_board(parent.board_rep)
+        raise NotImplementedError    
     
      
     def run_algorithm(self):
         while self.states:
             current_state: Node = self.get_current_state()
-            self.build_children(current_state)
-
-        # """
-        # Runs the algorithm untill all possible states are visited.
-        # """
-        # while self.states:
-        #     new_graph = self.get_next_state()
-
-        #     # Retrieve the next empty node.
-        #     node = new_graph.get_empty_node()
-
-        #     if node is not None:
-        #         self.build_children(new_graph, node)
-        #     else:
-        #         # Stop if we find a solution
-        #         # break
-
-        #         # or ontinue looking for better graph
-        #         self.check_solution(new_graph)
-
-        # # Update the input graph with the best result found.
-        # self.graph = self.best_solution
         
         
     
