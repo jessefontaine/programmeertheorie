0bf14f353472b7250de8bfc38daec779893f42c5
Annemarie <annemarie@MacBook-Air-van-Annemarie.local>
2022-06-23 12:52:38 +0200

    hill climber new

--- code/algorithms/hill_climber_new.py
+++ code/algorithms/hill_climber_new.py
@@ -4,6 +4,8 @@ from typing import Tuple, List, Set, Union
 from xml.dom.minicompat import NodeList
 from .random_alg_new import RandomAlg
 from .bfs import Bfs
+from .dfs import Dfs
+from .bdfs import Bdfs
 from code.classes import Board
 from code.functions.functions import write_moves_to_file
 from code.classes import Node
@@ -12,29 +14,43 @@ import random
 
 class HillClimberNew:
 
-    def __init__(self, board: Board, iteration: int, mode: str):
+    def __init__(self, board: Board, iteration: int, start_mode: str, improve_mode: str):
         self.board: Board = board
         self.iteration: int = iteration
-        self.node_list: List[Node] = self.node_list(mode)
-        # a = RandomAlg(self.board)
-        
-        # a.run_algorithm()
-        # print(a.moves_amount)
-        # self.node_list: List[Node] = a.node_list
+        self.improve_mode: str = improve_mode
+        self.node_list: List[Node] = self.begin_solution(self.make_algorithm(start_mode))
 
-    def begin_solution(self, mode: str):
+        # REMOVE LATER
+        print(len(self.node_list))
+
+    def make_algorithm(self, mode: str, start_node: Union[Node, None] = None, end_node: Union[Node, None] = None) -> Union[RandomAlg, Bfs, Dfs, Bdfs]:
         if mode == 'random':
             algorithm: Union[RandomAlg, Bfs] = RandomAlg(self.board)
         elif mode == 'breadth':
-            algorithm = Bfs(self.board, 300)
-        # elif mode == "depth":
-        #     algorithm = Dfs(board, 300)
-        # elif mode == "bestdepth":
-        #     algorithm = Bdfs(board, 300)
+            algorithm = Bfs(self.board, 300, start_node, end_node)
+        elif mode == "depth":
+            algorithm = Dfs(self.board, 300, start_node, end_node)
+        elif mode == "bestdepth":
+            algorithm = Bdfs(self.board, 300, start_node, end_node)
+
+        return algorithm
+
+    def begin_solution(self, algorithm: Union[RandomAlg, Bfs, Dfs, Bdfs]) -> List[Node]:
+        algorithm.run_algorithm()
+
         return algorithm.node_list
 
     def reset_algorithm(self):
         pass
+
+    def choose_interval(self) -> int:
+        interval: int = len(self.node_list)
+
+        # want interval that is not bigger then node list
+        while interval >= len(self.node_list):
+            interval = random.randint(5, 20)    #DEZE OOK NOG VARIABLE MAKEN!!!!!!!!
+
+        return interval
 
     def create_moves_made(self, start_node: Node, final_node: Node) -> None:
         self.moves_made: List[Tuple[str, int]] = []
@@ -49,28 +65,58 @@ class HillClimberNew:
         self.moves_made = self.moves_made[::-1]
 
     def run_algorithm(self) -> None:
-        for _ in range(100):
-            interval: int = len(self.node_list)
-
-            # want interval that is not bigger then node list
-            while interval >= len(self.node_list):
-                interval = random.randint(5, 20)
+        #MOET NOG EEN MAX OPKOMEN
+        for _ in range(self.iteration):
+            print('yess')
+            interval: int = self.choose_interval()
 
             # choose ranodm start point in node list
             start = random.randint(0, len(self.node_list) - interval - 1)
 
             # do algoritme on small part to get it better
-            b = Bfs(self.board, 300, self.node_list[start], self.node_list[start + interval])
-            b.run_algorithm()
+            alg = self.make_algorithm(self.improve_mode, self.node_list[start], self.node_list[start + interval])
+            alg.run_algorithm()
+            print(interval)
+            print(len(alg.node_list))
+
+            if interval <= len(alg.node_list):
+                break
 
             # put the new improved part of node list into the node list, different when you improve the very last part
             if start + interval == len(self.node_list) - 1:
-                self.node_list = self.node_list[:start] + b.node_list
+                self.node_list = self.node_list[:start] + alg.node_list
             else:
-                self.node_list[start + interval + 1].new_parent(b.node_list[-1])
+                self.node_list[start + interval + 1].new_parent(alg.node_list[-1])
         
-                self.node_list = self.node_list[:start] + b.node_list + self.node_list[start + interval + 1:]
+                self.node_list = self.node_list[:start] + alg.node_list + self.node_list[start + interval + 1:]
         
         self.create_moves_made(self.node_list[0], self.node_list[-1])
 
         print(self.moves_amount, " ")
+
+    # def run_algorithm(self) -> None:
+    #     for _ in range(self.iteration):
+    #         interval: int = len(self.node_list)
+
+    #         # want interval that is not bigger then node list
+    #         while interval >= len(self.node_list):
+    #             interval = random.randint(5, 20)
+
+    #         # choose ranodm start point in node list
+    #         start = random.randint(0, len(self.node_list) - interval - 1)
+
+    #         # do algoritme on small part to get it better
+    #         b = Bfs(self.board, 300, self.node_list[start], self.node_list[start + interval])
+    #         b.run_algorithm()
+
+    #         # put the new improved part of node list into the node list, different when you improve the very last part
+    #         if start + interval == len(self.node_list) - 1:
+    #             self.node_list = self.node_list[:start] + b.node_list
+    #         else:
+    #             self.node_list[start + interval + 1].new_parent(b.node_list[-1])
+        
+    #             self.node_list = self.node_list[:start] + b.node_list + self.node_list[start + interval + 1:]
+        
+    #     self.create_moves_made(self.node_list[0], self.node_list[-1])
+
+    #     print(self.moves_amount, " ")
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import os
 from typing import Union, List, Tuple
 
 from code.classes import Board
-from code.algorithms import RandomAlg, Bfs, Dfs, Bdfs, HillClimber, Random_Alg
+from code.algorithms import RandomAlg, Bfs, Dfs, Bdfs, HillClimber, Random_Alg, HillClimberNew
 from code.functions import batch_runner, plot_steps_to_file, steps_amount_to_file, write_moves_to_file
 
 
@@ -27,7 +27,7 @@ def main(infile: str, outfolder: str, mode: str, runs: int, output_moves: bool):
     elif mode == "bestdepth":
         algorithm = Bdfs(board, 300)
     elif mode == "hill":
-        algorithm = HillClimber(board)
+        algorithm = HillClimberNew(board, 10, 'random', 'bestdepth')
     else:
         print("TODO")
     
