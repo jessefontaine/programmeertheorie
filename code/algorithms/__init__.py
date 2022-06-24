2e20dd8306ea725b58c92fbc9cf5fe2e1de0be96
Laura <lhaverkorn@github.com>
2022-06-24 14:11:17 +0200

    name change HCR to HC

--- code/algorithms/__init__.py
+++ code/algorithms/__init__.py
@@ -2,6 +2,6 @@ from .random_algorithm import RandomAlg
 from .bfs import Bfs
 from .dfs import Dfs
 from .bdfs import Bdfs
-from .hill_climber_repeat import HCR
+from .hill_climber import HC
 from .restart_hill_climber import RHC
 from .Steepest_ascent_hill_climber import SHC
