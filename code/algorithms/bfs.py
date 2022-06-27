430981ca377115bcfe81fad474b833c7ea31c132
Laura <lhaverkorn@github.com>
2022-06-27 15:27:13 +0200

    alg black

--- code/algorithms/bfs.py
+++ code/algorithms/bfs.py
@@ -4,6 +4,11 @@ from code.algorithms.base_algorithms.tree_search import Treesearcher
 
 
 class Bfs(Treesearcher):
+    """
+    The Breadth First Search class that runs a search algorithm.
+    Searches a winning state for every layer of the tree.
+    """
+
     def get_current_state(self):
         """
         Gets first state from queue.
