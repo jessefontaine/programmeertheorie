abf573680055fef92383314f88c1b79a7e4fa171
Laura <lhaverkorn@github.com>
2022-06-27 14:43:06 +0200

    readme

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ def main(infile: str, outfolder: str, mode: str, runs: int, output_moves: bool):
         elif mode == "restarthill":
             plateau_iteration = 20
             algorithm = RHC(
-                board, runs, 4, 10, start_mode, improve_mode, plateau_iteration
+                board, runs, 4, 20, start_mode, improve_mode, plateau_iteration
             )
         elif mode == "sahill":
             algorithm = SA(board, runs, 4, 10, start_mode, improve_mode)
