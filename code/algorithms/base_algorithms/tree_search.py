c8f94c9a7881cac4c037410f163bd01cb3ca80e7
Laura <lhaverkorn@github.com>
2022-06-27 16:49:04 +0200

    Treesearcher header

--- code/algorithms/base_algorithms/tree_search.py
+++ code/algorithms/base_algorithms/tree_search.py
@@ -1,3 +1,17 @@
+"""
+tree_search.py
+
+Programmeertheorie Rush Hour
+
+Jesse Fontaine - 12693375
+Annemarie Geertsema - 12365009
+Laura Haverkorn - 12392707
+
+- Contains class Treesearch inherits BaseAlg (Base Algorithm).
+- Contains functions to build tree and sort children.
+- Contains functions to reset and run the algorithm.
+"""
+
 from __future__ import annotations
 
 import random
