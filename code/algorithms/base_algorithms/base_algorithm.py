a5c4afeff67685451edbea198a1c8d0cb6c5339b
jessefontaine <jesse.fontaine98@gmail.com>
2022-06-27 12:32:03 +0200

    set board dumbness

--- code/algorithms/base_algorithms/base_algorithm.py
+++ code/algorithms/base_algorithms/base_algorithm.py
@@ -31,13 +31,13 @@ class BaseAlg:
             self.find_win = False
             self.end_node: Node = end_node
 
-    def check_finished(self, state: Node) -> bool:
+    def check_finished(self, state: Node, set_board_every_check: bool = True) -> bool:
         """
         Checks whether a given state satisfies the constraints.
         """
-
-        # setup the board according to the given state
-        self.board.set_board(state.board_rep)
+        if set_board_every_check:
+            # setup the board according to the given state
+            self.board.set_board(state.board_rep)
 
         # return whether contraints are satisfied
         if self.find_win:
