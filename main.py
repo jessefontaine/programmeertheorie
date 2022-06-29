e96a233ed6170a13d87f77f1be84ab848b346f91
Laura <lhaverkorn@github.com>
2022-06-29 16:02:45 +0200

    InvalidAlgorithmError

--- main.py
+++ main.py
@@ -60,6 +60,13 @@ def main(infile: str, outfolder: str, mode: str, runs: int, output_moves: bool):
             mode.split("/")[2],
         )
 
+        if mode not in ["hill", "restarthill", "sahill"]:
+            raise InvalidAlgorithmError("Given algorithm does not exist.")
+        elif start_mode not in ["random", "breadth", "depth", "bestdepth"]:
+            raise InvalidAlgorithmError("Given algorithm does not exist or can not be used to find a starting solution.")
+        elif improve_mode not in ["random", "breadth", "depth", "bestdepth"]:
+            raise InvalidAlgorithmError("Given algorithm does not exist or can not be used to improve.")
+
         if mode == "hill":
             iterative_algorithm: Union[HC, RHC, SA] = HC(
                 board, runs, MIN_INTERVAL, MAX_INTERVAL, start_mode, improve_mode
@@ -79,7 +86,7 @@ def main(infile: str, outfolder: str, mode: str, runs: int, output_moves: bool):
                 board, runs, MIN_INTERVAL, MAX_INTERVAL, start_mode, improve_mode
             )
     else:
-        raise InvalidAlgorithmError("Given algorithm does not exist")
+        raise InvalidAlgorithmError("Given algorithm does not exist.")
 
     try:
         os.makedirs(outfolder)
